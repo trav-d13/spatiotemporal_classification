f3563f6782ce92908412cdb8c48283d5e1423869
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-02-07 18:46:07 +0100

    Dataframe construction successful

--- src/data/RawDataAggregation.py
+++ src/data/RawDataAggregation.py
@@ -1,6 +1,18 @@
 import pandas as pd
 from Config import Definitions
 
+dataset = "observations_1.csv"
+observation_path = "/data/raw/"
+
+
+def generate_dataframe(observation_file) -> pd.DataFrame:
+    file_path = Definitions.root_dir() + \
+               observation_path + \
+               observation_file
+    df = pd.read_csv(file_path, sep=",")
+    return df
+
+
 if __name__ == "__main__":
-    print(Definitions.root_dir())
+    generate_dataframe(dataset)
 
