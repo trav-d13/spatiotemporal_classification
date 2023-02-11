7311fcb792cb82d67c3b5595b23723b5cf092c79
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-02-11 14:08:53 +0100

    Date formatting and testing completed. Method requires documentation still

--- src/data/DataCleanPipeline.py
+++ src/data/DataCleanPipeline.py
@@ -1,5 +1,8 @@
 import pandas as pd
+
 from Config import root_dir
+
+from datetime import datetime
 
 
 class Pipeline:
@@ -18,14 +21,13 @@ class Pipeline:
             self.datasets = datasets
             self.resource_path = root_dir() + "/data/raw/"
         else:
-            self.df = test_df
+            self.df = test_df.copy(deep=True)
 
     def aggregate_observations(self):
         """Method aggregates all observations from separate files, placing them within a df for manipulation"""
         for dataset in self.datasets:
             df_temp = pd.read_csv(self.resource_path + dataset)
             self.df = pd.concat([self.df, df_temp])
-        print(self.df.head())
 
     def enforce_unique_ids(self):
         """Removal of any duplicate observations utilizing their observation id
@@ -33,6 +35,15 @@ class Pipeline:
         In place duplicate removal such that changes are effected directly within df
         """
         self.df.drop_duplicates(subset=['id'], keep='first', inplace=True)
+
+    def format_observation_dates(self):
+        self.df['observed_on'] = pd.to_datetime(self.df['observed_on'],
+                                                format='%Y-%m-%d',
+                                                yearfirst=True,
+                                                errors='coerce',
+                                                exact=True).astype(str)
+        self.df.query('observed_on != "NaT"', inplace=True)
+        self.df.reset_index(drop=True, inplace=True)
 
 
 if __name__ == "__main__":
--- tests/test_cleaning_pipeline.py
+++ tests/test_cleaning_pipeline.py
@@ -5,19 +5,12 @@ from src.data.DataCleanPipeline import Pipeline
 
 test_data = [[128984633, "2022-08-02"],
              [129048796, "2022-04-02"],
-             [110157830, "2022-04-02"],
+             [110157830, "2022-04/02"],
              [110266861, "2022-08-02"],
-             [111311033, "2022-09-15"],
-             [111311033, "2022-09-15"]]
-
-correct_data = [[128984633, "2022-08-02"],
-                [129048796, "2022-04-02"],
-                [110157830, "2022-04-02"],
-                [110266861, "2022-08-02"],
-                [111311033, "2022-09-15"]]
+             [111311033, "202g-09-15"],
+             [111311033, "2022-08-15"]]
 
 test_df = pd.DataFrame(test_data, columns=['id', 'observed_on'])
-correct_df = pd.DataFrame(correct_data, columns=['id', 'observed_on'])
 
 
 class TestCleaningPipeline(unittest.TestCase):
@@ -25,7 +18,30 @@ class TestCleaningPipeline(unittest.TestCase):
     def test_unique_id(self):
         pipeline = Pipeline(test_df=test_df)
         pipeline.enforce_unique_ids()
+
+        correct_data = [[128984633, "2022-08-02"],
+                        [129048796, "2022-04-02"],
+                        [110157830, "2022-04/02"],
+                        [110266861, "2022-08-02"],
+                        [111311033, "202g-09-15"]]
+        correct_df = pd.DataFrame(correct_data, columns=['id', 'observed_on'])
+
         self.assertTrue(correct_df.equals(pipeline.df))
+
+    def test_data_formatting(self):
+        pipeline = Pipeline(test_df=test_df)
+        pipeline.format_observation_dates()
+
+        correct_data = [[128984633, "2022-08-02"],
+                        [129048796, "2022-04-02"],
+                        [110266861, "2022-08-02"],
+                        [111311033, "2022-08-15"]]
+        correct_df = pd.DataFrame(correct_data, columns=['id', 'observed_on'])
+
+        self.assertTrue(correct_df.equals(pipeline.df))
+
+
+
 
 
 if __name__ == '__main__':
