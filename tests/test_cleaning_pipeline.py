fcc13654f7937113856abc706f672b53b6e686f2
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-02-11 12:35:14 +0100

    Unique id's are enforced and duplicates removed. Test confirms function is working

--- tests/test_cleaning_pipeline.py
+++ tests/test_cleaning_pipeline.py
@@ -1,8 +1,32 @@
 import pandas as pd
+import unittest
 
-test_data = [[128984633, ""],
-             [129048796, ""],
-             [110157830, ""],
-             [110266861, ""],
-             [111311033, ""]]
+from src.data.DataCleanPipeline import Pipeline
+
+test_data = [[128984633, "2022-08-02"],
+             [129048796, "2022-04-02"],
+             [110157830, "2022-04-02"],
+             [110266861, "2022-08-02"],
+             [111311033, "2022-09-15"],
+             [111311033, "2022-09-15"]]
+
+correct_data = [[128984633, "2022-08-02"],
+                [129048796, "2022-04-02"],
+                [110157830, "2022-04-02"],
+                [110266861, "2022-08-02"],
+                [111311033, "2022-09-15"]]
+
 test_df = pd.DataFrame(test_data, columns=['id', 'observed_on'])
+correct_df = pd.DataFrame(correct_data, columns=['id', 'observed_on'])
+
+
+class TestCleaningPipeline(unittest.TestCase):
+    # Test duplicate observation (unique id) removal
+    def test_unique_id(self):
+        pipeline = Pipeline(test_df=test_df)
+        pipeline.enforce_unique_ids()
+        self.assertTrue(correct_df.equals(pipeline.df))
+
+
+if __name__ == '__main__':
+    unittest.main()
