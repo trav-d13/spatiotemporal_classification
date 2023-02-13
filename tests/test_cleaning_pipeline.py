c3383bbb5551a06abe38d7a0882e4c246a9ea0ab
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-02-13 09:49:19 +0100

    All tests passing. Slight test refactor required

--- tests/test_cleaning_pipeline.py
+++ tests/test_cleaning_pipeline.py
@@ -15,19 +15,19 @@ test_df = pd.DataFrame(test_data, columns=['id',
                                            'latitude',
                                            'longitude'])
 
-
+# TODO Refactor tests to only check modifed parameters not entire dataframe
 class TestCleaningPipeline(unittest.TestCase):
     # Test duplicate observation (unique id) removal
     def test_unique_id(self):
         pipeline = Pipeline(test_df=test_df)
         pipeline.enforce_unique_ids()
 
-        correct_data = [[128984633, "2022-08-02"],
-                        [129048796, "2022-04-02"],
-                        [110157830, "2022-04/02"],
-                        [110266861, "2022-08-02"],
-                        [111311033, "202g-09-15"]]
-        correct_df = pd.DataFrame(correct_data, columns=['id', 'observed_on'])
+        correct_data = [[128984633, "2022-08-02", -30.4900714453, 151.6392706226],
+                        [129048796, "2022-04-02", 25.594095, 85.137566],
+                        [110157830, "2022-04/02", -33.918861, 18.423300],
+                        [110266861, "2022-08-02", -38.6714966667, 178.0182195],
+                        [111311033, "202g-09-15", -31.416668, -64.183334]]
+        correct_df = pd.DataFrame(correct_data, columns=['id', 'observed_on', 'latitude', 'longitude'])
 
         self.assertTrue(correct_df.equals(pipeline.df))
 
@@ -35,11 +35,11 @@ class TestCleaningPipeline(unittest.TestCase):
         pipeline = Pipeline(test_df=test_df)
         pipeline.format_observation_dates()
 
-        correct_data = [[128984633, "2022-08-02"],
-                        [129048796, "2022-04-02"],
-                        [110266861, "2022-08-02"],
-                        [111311033, "2022-08-15"]]
-        correct_df = pd.DataFrame(correct_data, columns=['id', 'observed_on'])
+        correct_data = [[128984633, "2022-08-02", -30.4900714453, 151.6392706226],
+                        [129048796, "2022-04-02", 25.594095, 85.137566],
+                        [110266861, "2022-08-02", -38.6714966667, 178.0182195],
+                        [111311033, "2022-08-15", 51.260197, 4.402771]]
+        correct_df = pd.DataFrame(correct_data, columns=['id', 'observed_on', 'latitude', 'longitude'])
 
         self.assertTrue(correct_df.equals(pipeline.df))
 
